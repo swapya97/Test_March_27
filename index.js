f36692cf2fed6e7fc55440c450ee040164000276
swapya97 <91781600+swapya97@users.noreply.github.com>
2022-03-06 19:36:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function show_productDetails(id){
+    document.getElementById(id).classList.replace("hide","product-details");
+
+}
+function close_productList(id){
+    document.getElementById(id).classList.add("hide");
+}
